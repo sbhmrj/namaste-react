7be5d6f46543294669f495f24baf17b69c0ab779
Subham Raj <subham4151raj@gmail.com>
2024-10-01 17:06:28 +0530

    update

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * <div id="child2">
- *         <h1>I'm h1 tag</h1>
- *         <h2>I'm h1 tag</h2>
- *      </div>
- * </div>
- *
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
+      React.createElement("h1", {}, "This is Namaste React"),
+      React.createElement("h2", {}, "By Subham Raj"),
     ]),
     React.createElement("div", { id: "child2" }, [
       React.createElement("h1", {}, "I'm an h1 tag"),
@@ -25,9 +12,9 @@ const parent = React.createElement("div", { id: "parent" }, [
     ]),
   ]);
   
-  //JSX
+
   
-  console.log(parent); // object
+  console.log(parent); 
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
   
